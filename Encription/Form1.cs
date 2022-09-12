c3e33aec6c584a5fe7bcb3fad89400bc68cd7cb4
IceTeaCan <71415229+IceTeaCan@users.noreply.github.com>
2022-09-11 19:27:19 -0500

    Add files via upload

---
+++ Encription/Form1.cs
@@ -0,0 +1,44 @@
+﻿
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  -------------------     Proyecto de desencripción de mensaje con valor agregado de encripción y simplificación de funciones                 ---------------------
+  -------------------     Autor : Jonathan Salazar Rodríguez                                                                                  ---------------------
+  -------------------     Profesor : Aarón Barquero Serrano                                                                                   ---------------------
+  -------------------     Materia : Diseño y desarrollo de aplicaciones comerciales                           Fecha entrega : 4/6/2021        ---------------------
+  -------------------                                                                                                                         ---------------------
+  -----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+using System;
+using System.Windows.Forms;
+using EncriptProcess;
+
+namespace Encription
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Negocios word = new Negocios();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_out.Text =  word.En(tb_IN.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tb_out.Text = word.De(tb_IN.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tb_out.Text = word.DeRes(tb_IN.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tb_out.Text = word.EnRes(tb_IN.Text);
+
+        }
+    }
+}
